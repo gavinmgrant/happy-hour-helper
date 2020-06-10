87c7ae946da9465fff6985cc397ca83fa8fcb304
Gavin Grant <gavingrant@gmail.com>
2020-06-10 09:40:20 -0700

    shuffled arrays and updated format of results

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ const apiKeyCocktail = '1';
 const searchURLFood = 'https://api.edamam.com/search';
 const searchURLCocktail = 'https://www.thecocktaildb.com/api/json/v1/';
 
+// This shuffles an array.
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
 
 function displayFoodResults(responseJson) {
     console.log(responseJson);
     $('#food-results-list').empty();
-  
+
     if (responseJson.count == 0) {
       $('#food-error').removeClass('hidden');
       $('#food-results').addClass('hidden');
       } else {
+        shuffle(responseJson.hits);
         for (let i = 0; i < responseJson.hits.length; i++){
             $('#food-results-list').append(
                 `<li><h3>${responseJson.hits[i].recipe.label}</h3>
                 <img class="images" src ="${responseJson.hits[i].recipe.image}">
-                <p>Serves: ${responseJson.hits[i].recipe.yield}  |  Calories: ${Math.round(responseJson.hits[i].recipe.calories)}</p>
                 <p>Ingredients needed:</p>
                 </li>`)
             for (let j = 0; j < responseJson.hits[i].recipe.ingredientLines.length; j++){
@@ -27,41 +35,12 @@ function displayFoodResults(responseJson) {
                     `<li>${responseJson.hits[i].recipe.ingredientLines[j]}</li>`)
                 }
             $('#food-results-list').append(
-                `<p>For directions to this recipe, go to <a href="${responseJson.hits[i].recipe.url}" target="_blank">${responseJson.hits[i].recipe.source}.</p>`)
+                `<p>Serves: ${responseJson.hits[i].recipe.yield}  |  Calories: ${Math.round(responseJson.hits[i].recipe.calories)}</p>
+                <p>For directions to this recipe, go to <a href="${responseJson.hits[i].recipe.url}" target="_blank">${responseJson.hits[i].recipe.source}.</p>`)
             }; 
         
         $('#food-results').removeClass('hidden');
         $('#food-error').addClass('hidden');
-        $('#start-over').removeClass('hidden');
-    }
-}
-
-function displayCocktailResults(responseJson) {
-    console.log(responseJson);
-    $('#cocktail-results-list').empty();
-
-    if (responseJson.drinks == null) {
-        $('#cocktail-error').removeClass('hidden');
-        $('#cocktail-results').addClass('hidden');
-        } else {
-            for (let i = 0; i < responseJson.drinks.length; i++) {
-                $('#cocktail-results-list').append(
-                    `<li><h3>${responseJson.drinks[i].strDrink}</h3>
-                    <img class="images" src ="${responseJson.drinks[i].strDrinkThumb}">
-                    <p>${responseJson.drinks[i].strInstructions}</p>
-                    <p>Ingredients needed:</p>
-                    <li>${responseJson.drinks[i].strMeasure1} - ${responseJson.drinks[i].strIngredient1}</li>
-                    <li>${responseJson.drinks[i].strMeasure2} - ${responseJson.drinks[i].strIngredient2}</li>
-                    <li>${responseJson.drinks[i].strMeasure3} - ${responseJson.drinks[i].strIngredient3}</li>
-                    <li>${responseJson.drinks[i].strMeasure4} - ${responseJson.drinks[i].strIngredient4}</li>
-                    <li>${responseJson.drinks[i].strMeasure5} - ${responseJson.drinks[i].strIngredient5}</li>
-                    <li>${responseJson.drinks[i].strMeasure6} - ${responseJson.drinks[i].strIngredient6}</li>
-                    <li>${responseJson.drinks[i].strMeasure7} - ${responseJson.drinks[i].strIngredient7}</li>
-                    </li>`)
-            };
-
-        $('#cocktail-results').removeClass('hidden');
-        $('#cocktail-error').addClass('hidden');
         $('#start-over').removeClass('hidden');
     }
 }
@@ -72,19 +51,19 @@ function getFoodRestrictions(gf, ve, vg) {
     let veRestriction = '';
     let vgRestriction = '';
 
-    if (gf === true) {
+    if (gf) {
         gfRestriction = '&health=gluten-free';
     } else {
         gfRestriction = '';
     };
 
-    if (ve === true) {
+    if (ve) {
         veRestriction = '&health=vegan';
     } else {
         veRestriction = '';
     };
 
-    if (vg === true) {
+    if (vg) {
         vgRestriction = '&health=vegetarian';
     } else {
         vgRestriction = '';
@@ -111,6 +90,45 @@ function getFood(query, maxResults=3) {
     }
 }
 
+function displayCocktailResults(responseJson) {
+    console.log(responseJson);
+    $('#cocktail-results-list').empty();
+
+    if (responseJson.drinks == null) {
+        $('#cocktail-error').removeClass('hidden');
+        $('#cocktail-results').addClass('hidden');
+        } else {
+            shuffle(responseJson.drinks);
+            for (let i = 0; i < responseJson.drinks.length; i++) {
+                $('#cocktail-results-list').append(
+                    `<li><h3>${responseJson.drinks[i].strDrink}</h3>
+                    <img class="images" src ="${responseJson.drinks[i].strDrinkThumb}">
+                    <p>Ingredients needed:</p>
+                    <li><span class="${responseJson.drinks[i].strMeasure1}">${responseJson.drinks[i].strMeasure1} </span><span class="${responseJson.drinks[i].strIngredient1}">${responseJson.drinks[i].strIngredient1}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure2}">${responseJson.drinks[i].strMeasure2} </span><span class="${responseJson.drinks[i].strIngredient2}">${responseJson.drinks[i].strIngredient2}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure3}">${responseJson.drinks[i].strMeasure3} </span><span class="${responseJson.drinks[i].strIngredient3}">${responseJson.drinks[i].strIngredient3}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure4}">${responseJson.drinks[i].strMeasure4} </span><span class="${responseJson.drinks[i].strIngredient4}">${responseJson.drinks[i].strIngredient4}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure5}">${responseJson.drinks[i].strMeasure5} </span><span class="${responseJson.drinks[i].strIngredient5}">${responseJson.drinks[i].strIngredient5}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure6}">${responseJson.drinks[i].strMeasure6} </span><span class="${responseJson.drinks[i].strIngredient6}">${responseJson.drinks[i].strIngredient6}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure7}">${responseJson.drinks[i].strMeasure7} </span><span class="${responseJson.drinks[i].strIngredient7}">${responseJson.drinks[i].strIngredient7}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure8}">${responseJson.drinks[i].strMeasure8} </span><span class="${responseJson.drinks[i].strIngredient8}">${responseJson.drinks[i].strIngredient8}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure9}">${responseJson.drinks[i].strMeasure9} </span><span class="${responseJson.drinks[i].strIngredient9}">${responseJson.drinks[i].strIngredient9}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure10}">${responseJson.drinks[i].strMeasure10} </span><span class="${responseJson.drinks[i].strIngredient10}">${responseJson.drinks[i].strIngredient10}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure11}">${responseJson.drinks[i].strMeasure11} </span><span class="${responseJson.drinks[i].strIngredient11}">${responseJson.drinks[i].strIngredient11}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure12}">${responseJson.drinks[i].strMeasure12} </span><span class="${responseJson.drinks[i].strIngredient12}">${responseJson.drinks[i].strIngredient12}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure13}">${responseJson.drinks[i].strMeasure13} </span><span class="${responseJson.drinks[i].strIngredient13}">${responseJson.drinks[i].strIngredient13}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure14}">${responseJson.drinks[i].strMeasure14} </span><span class="${responseJson.drinks[i].strIngredient14}">${responseJson.drinks[i].strIngredient14}</span></li>
+                    <li><span class="${responseJson.drinks[i].strMeasure15}">${responseJson.drinks[i].strMeasure15} </span><span class="${responseJson.drinks[i].strIngredient15}">${responseJson.drinks[i].strIngredient15}</span></li>
+                    <p>${responseJson.drinks[i].strInstructions}</p>
+                    </li>`);
+                };        
+            };
+        
+        $('#cocktail-results').removeClass('hidden');
+        $('#cocktail-error').addClass('hidden');
+        $('#start-over').removeClass('hidden');
+}
+
 function getCocktails(query) {
     const url = searchURLCocktail + apiKeyCocktail + '/search.php?s=' + query;
     console.log(url);
@@ -128,21 +146,21 @@ function getCocktails(query) {
 }
 
 function displayOptions(food, cocktails) {
-    if (food === true) {
+    if (food) {
         $('#food-options').removeClass('hidden');
         $('#options-button').removeClass('hidden');
         $('#landing').addClass('hidden');
     } else {
         $('#food-options').addClass('hidden');
     };
-    if (cocktails === true) {
+    if (cocktails) {
         $('#cocktail-options').removeClass('hidden');
         $('#options-button').removeClass('hidden');
         $('#landing').addClass('hidden');
     } else {
         $('#cocktail-options').addClass('hidden');
     };
-    if (food === false && cocktails === false) {
+    if (!food && !cocktails) {
         $('#options-button').addClass('hidden');
         alert('You must pick at least one option.');
     }
